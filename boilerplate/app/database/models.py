2e9fe93c251358e1edca1fcc048d0791d59a853c
Jenkins <jenkins@build.paas.intranet>
2017-04-06 17:14:50 +0200

    some cleanup

---
+++ boilerplate/app/database/models.py
@@ -0,0 +1,19 @@
+# The examples in this file come from the Flask-SQLAlchemy documentation
+# For more information take a look at:
+# http://flask-sqlalchemy.pocoo.org/2.1/quickstart/#simple-relationships
+
+from datetime import datetime
+
+from database import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.name
